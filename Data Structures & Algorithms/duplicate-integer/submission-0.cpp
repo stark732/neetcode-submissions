0efee504b79f5046f39152063750e938a5f39d8d
stark732 <106365909+stark732@users.noreply.github.com>
2026-09-20 02:49:45 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
+
+        for(int i = 0; i<nums.size(); i++){
+            if(mp.find(nums[i]) != mp.end()){
+                return true;
+            }
+            mp.insert({nums[i],i});
+        }
+        return false;
+    }
+};
